a6e88a05d9ddf9b09af6ece31f1feed88cc8f790
ZumbaCode <100531041+ZumbaCode@users.noreply.github.com>
2022-02-28 19:36:45 -0500

    Add files via upload

---
+++ Day 1/src/Main.py
@@ -0,0 +1,6 @@
+print("Welcome to Band Name Generator")
+
+nameOfBand = input("What is the city you new up In: ")
+petsName = input("What is the name of your Pet: ")
+
+print("Your Band Name Should be Called " + nameOfBand + " " + petsName)
